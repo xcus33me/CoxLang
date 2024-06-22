7bf756d8dc3fbcfad8a421c76c0e653e993faa2d
xcus33me <HarvieOrigin@yandex.ru>
2024-06-22 12:14:24 +0300

    Fixed implicit cast

--- src/Error.cpp
+++ src/Error.cpp
@@ -3,11 +3,11 @@
 #include "fmt/core.h"
 
 namespace ErrorReporter {
-    void Error(int line, const std::string& message) {
+    void Error(size_t line, const std::string& message) {
         Report(line, "", message);
     }
 
-    void Report(int line, const std::string& where, const std::string& message) {
+    void Report(size_t line, const std::string& where, const std::string& message) {
         // Needs something like
         // Error: Unexpected "," in argument list.
         //
