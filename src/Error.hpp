24b47eaef6fc5c0a4319129d835c2bc484a31f23
xcus33me <HarvieOrigin@yandex.ru>
2024-06-22 12:19:16 +0300

    fix

--- src/Error.hpp
+++ src/Error.hpp
@@ -8,6 +8,6 @@
 namespace ErrorReporter {
     inline bool had_error = false;
 
-    void Error(int line, const std::string& message);
-    void Report(int line, const std::string& where, const std::string& message);
+    void Error(size_t line, const std::string& message);
+    void Report(size_t line, const std::string& where, const std::string& message);
 }
