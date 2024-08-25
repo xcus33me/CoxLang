f207c4d3e9b8f817e6028c51ef29c5b1d2d843ab
xcus33me <HarvieOrigin@yandex.ru>
2024-08-25 16:04:45 +0300

    some: added implementation

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,14 +1,9 @@
 #include <vector>
 
 #include "parser.hpp"
+#include "token.hpp"
 
-Parser::Parser(std::vector<Token> tokens):
-    tokens_(std::move(tokens)),
-    current_(0) { }
 
-bool Parser::match(std::vector<TokenType> types) const {
-
-}
 
 std::unique_ptr<Expr> Parser::expression() {
     return equality();
@@ -23,6 +18,42 @@ std::unique_ptr<Expr> Parser::comparation() {
 
 }
 
+Parser::Parser(std::vector<Token> tokens):
+    tokens_(std::move(tokens)),
+    current_(0) { }
+
+bool Parser::match(std::vector<TokenType> types) {
+    for (const auto& type : types) {
+        if (check(type)) {
+            advance();
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Parser::is_at_end() const {
+    return peek().type_ == TokenType::EOF_TOKEN;
+}
+
+
+bool Parser::check(TokenType type) const {
+    if (is_at_end()) return false;
+    return peek().type_ == type;
+}
+
+Token Parser::advance() {
+    if (!is_at_end()) ++current_;
+    return previous();
+}
+
+Token Parser::peek() const {
+    return tokens_[current_];
+}
+
+Token Parser::previous() const {
+    return tokens_[current_ - 1];
+}
 
 
 
--- src/parser.hpp
+++ src/parser.hpp
@@ -14,7 +14,13 @@ private:
     std::vector<Token> tokens_;
     size_t current_ = 0;
 
-    [[nodiscard]] bool match(std::vector<TokenType> types) const;
+    [[nodiscard]] bool match(std::vector<TokenType> types);
+    [[nodiscard]] bool is_at_end() const;
+    [[nodiscard]] bool check(TokenType type) const;
+
+    [[nodiscard]] Token advance();
+    [[nodiscard]] Token peek() const;
+    [[nodiscard]] Token previous() const;
 
     std::unique_ptr<Expr> expression();
     std::unique_ptr<Expr> equality();
