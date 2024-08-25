5f6a52d8552422d75ccea6f74fbeb4bea33a071c
xcus33me <HarvieOrigin@yandex.ru>
2024-08-26 01:03:48 +0300

    feat: added implementation

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,28 +1,14 @@
 #include <vector>
 
+#include "expr.hpp"
 #include "parser.hpp"
 #include "token.hpp"
-
-
-
-std::unique_ptr<Expr> Parser::expression() {
-    return equality();
-}
-
-std::unique_ptr<Expr> Parser::equality() {
-    std::unique_ptr<Expr> expr = comparation();
-
-}
-
-std::unique_ptr<Expr> Parser::comparation() {
-
-}
 
 Parser::Parser(std::vector<Token> tokens):
     tokens_(std::move(tokens)),
     current_(0) { }
 
-bool Parser::match(std::vector<TokenType> types) {
+bool Parser::match(std::initializer_list<TokenType> types) {
     for (const auto& type : types) {
         if (check(type)) {
             advance();
@@ -54,6 +40,93 @@ Token Parser::peek() const {
 Token Parser::previous() const {
     return tokens_[current_ - 1];
 }
+
+Token Parser::consume(TokenType type, std::string message) {
+    if (check(type)) return advance();
+    throw ParseError(peek(), message);
+}
+
+std::unique_ptr<Expr> Parser::expression() {
+    return equality();
+}
+
+std::unique_ptr<Expr> Parser::equality() {
+    std::unique_ptr<Expr> expr = comparation();
+
+    while (match({TokenType::BANG_EQUAL, TokenType::EQUAL_EQUAL})) {
+        Token op = previous();
+        std::unique_ptr<Expr> right = comparation();
+        expr = std::make_unique<BinaryExpr>(expr, op, right);
+    }
+
+    return expr;
+}
+
+std::unique_ptr<Expr> Parser::comparation() {
+    std::unique_ptr<Expr> expr = term();
+
+    while (match({TokenType::GREATER, TokenType::GREATER_EQUAL, TokenType::LESS, TokenType::LESS_EQUAL})) {
+        Token op = previous();
+        std::unique_ptr<Expr> right = term();
+        expr = std::make_unique<BinaryExpr>(expr, op, right);
+    }
+
+    return expr;
+}
+
+std::unique_ptr<Expr> Parser::term() {
+    std::unique_ptr<Expr> expr = factor();
+
+    while (match({TokenType::MINUS, TokenType::PLUS})) {
+        Token op = previous();
+        std::unique_ptr<Expr> right = factor();
+        expr = std::make_unique<BinaryExpr>(expr, op, right);
+    }
+
+    return expr;
+}
+
+std::unique_ptr<Expr> Parser::factor() {
+    std::unique_ptr<Expr> expr = unary();
+
+    while (match({TokenType::SLASH, TokenType::STAR})) {
+        Token op = previous();
+        std::unique_ptr<Expr> right = unary();
+        expr = std::make_unique<BinaryExpr>(expr, op, right);
+    }
+
+    return expr;
+}
+
+std::unique_ptr<Expr> Parser::unary() {
+    if (match({TokenType::BANG, TokenType::MINUS})) {
+        Token op = previous();
+        std::unique_ptr<Expr> expr = unary();
+        return std::make_unique<UnaryExpr>(expr, op);
+    }
+
+    return primary();
+}
+
+std::unique_ptr<Expr> Parser::primary() {
+    if (match({TokenType::FALSE})) return std::make_unique<LiteralExpr>(false);
+    if (match({TokenType::TRUE})) return std::make_unique<LiteralExpr>(true);
+    if (match({TokenType::NIL})) return std::make_unique<LiteralExpr>(nullptr);
+
+    if (match({TokenType::NUMBER, TokenType::STRING})) {
+        return std::make_unique<LiteralExpr>(previous().literal_);
+    }
+
+    if (match({TokenType::LEFT_PAREN})) {
+        std::unique_ptr<Expr> expr = expression();
+        consume(TokenType::RIGHT_PAREN, "Expected ')' after expression.");
+        return std::make_unique<GroupingExpr>(expr);
+    }
+
+    throw ParseError(peek(), "Expect expression.");
+}
+
+
 
 
 
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,9 +1,25 @@
 #pragma once
+
+#include <logger.hpp>
 #include <memory>
 #include <vector>
 
 #include "expr.hpp"
 #include "token.hpp"
+
+struct ParseError : public std::runtime_error {
+    ParseError(const Token& token, const std::string& message) : std::runtime_error(message) {
+        ErrorReporter::error(token, message);
+    };
+};
+
+/// expression     → equality ;
+/// equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+/// comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+/// term           → factor ( ( "-" | "+" ) factor )* ;
+/// factor         → unary ( ( "/" | "*" ) unary )* ;
+/// unary          → ( "!" | "-" ) unary | primary ;
+/// primary        → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
 
 class Parser {
 public:
@@ -14,15 +30,21 @@ private:
     std::vector<Token> tokens_;
     size_t current_ = 0;
 
-    [[nodiscard]] bool match(std::vector<TokenType> types);
+    [[nodiscard]] bool match(std::initializer_list<TokenType> types);
     [[nodiscard]] bool is_at_end() const;
     [[nodiscard]] bool check(TokenType type) const;
 
-    [[nodiscard]] Token advance();
+    Token advance();
     [[nodiscard]] Token peek() const;
     [[nodiscard]] Token previous() const;
+
+    Token consume(TokenType type, std::string message);
 
     std::unique_ptr<Expr> expression();
     std::unique_ptr<Expr> equality();
     std::unique_ptr<Expr> comparation();
+    std::unique_ptr<Expr> term();
+    std::unique_ptr<Expr> factor();
+    std::unique_ptr<Expr> unary();
+    std::unique_ptr<Expr> primary();
 };
